9df9857ea2f22b3c9f37ec058da798c2918647d9
adriraw <adrianrijorijo@gmail.com>
2026-05-28 21:33:15 +0100

    Resuelto 2.3

--- Tema2_CalculoCeros/P23_COVID_Newton_Raphson_hacer_alumno/mn_calculo_ceros.cpp
+++ Tema2_CalculoCeros/P23_COVID_Newton_Raphson_hacer_alumno/mn_calculo_ceros.cpp
@@ -5,34 +5,34 @@
 using namespace std;
 
 /// PARAMETROS DE LA DISTRIBUCION GAMMA
-real alfa,beta,d;
+real alfa,beta1,d;
 
 /// DISTRIBUCIÓN GAMMA
 real Gamma(real x){
   if(x<0) return 0;
-  return d*pow(x,alfa-1.)*exp(-beta*x);
+  return d*pow(x,alfa-1.)*exp(-beta1*x);
 }
 
 /// DERIVADA DE LA DISTRIBUCIÓN GAMMA
 real Gammap(real x){
   if(x<0) return 0;
-  return d*(alfa-1)*pow(x,alfa-2.)*exp(-beta*x)-d*beta*pow(x,alfa-1.)*exp(-beta*x);
+  return d*(alfa-1)*pow(x,alfa-2.)*exp(-beta1*x)-d*beta1*pow(x,alfa-1.)*exp(-beta1*x);
 }
 
 /// CALCULO DE LOS PARÁMETROS DE LA DISTRIBUCIÓN GAMMA A PARTIR DE LA
 /// MEDIA Y VARIANZA MUESTRAL
 void calculo_parametros_Gamma(real media, real varianza){
-  /// calculo de alfa y beta
-  beta=media/varianza;
-  alfa=media*beta;
+  /// calculo de alfa y beta1
+  beta1=media/varianza;
+  alfa=media*beta1;
 
   /// calculo de d a través de la integral (se verá en el tema 5)
   real h=0.001;
   real suma=0;
-  for(real x=0;x<100;x+=h) suma+=h*pow(x,alfa-1.)*exp(-beta*x);
+  for(real x=0;x<100;x+=h) suma+=h*pow(x,alfa-1.)*exp(-beta1*x);
   d=1./suma;
 
-  cout << "\nalfa = " << alfa << " beta = " << beta << " d = " << d << "\n";
+  cout << "\nalfa = " << alfa << " beta1 = " << beta1 << " d = " << d << "\n";
 
 }
 
@@ -46,7 +46,26 @@ int NiterMax, /// número de iteraciones máximo
 real TOL) /// tolerancia para parar el algoritmo
 {
   /// HACER ALUMNO
+    real f_x0 = f(x0);
+    real x1;
+    real derivada;
+    for(int Niter = 0; Niter < NiterMax; Niter++){
 
+        if(f_x0 == 0) return Niter;
+
+        derivada = mn_derivada1(f, x0);
+        if(derivada == 0) return Niter;
+
+        x1 = x0 - (f_x0 / derivada);
+        f_x0 = f(x1);
+
+        if(mn_distancia(x1, x0) < TOL){
+            x0 = x1;
+            return Niter + 1;
+        }
+        x0 = x1;
+    }
+    return -1;
 }
 
 
