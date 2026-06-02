39cc7c27a5fbaa39a6d5cbd44c44e7bad8820336
adriraw <adrianrijorijo@gmail.com>
2026-06-02 21:43:54 +0100

    3.2 hecho - Interpolacion lineal

--- Tema3_Interpolacion_I/P32_lineal_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P32_lineal_hacer_alumno/mn_interpolacion.cpp
@@ -9,6 +9,23 @@ real x0)  /// punto donde se evalua la función interpolada
 {
   /// HACER ALUMNO
 
+    int N = x.dim();
+
+    if(x0 <= x[0]){
+        return f[0] + ((f[1] - f[0])/(x[1] - x[0])) * (x0 - x[0]);
+    }
+
+    if(x0 >= x[N - 1]){
+        return f[N - 2] + ((f[N - 1] - f[N - 2])/(x[N - 1] - x[N - 2])) * (x0 - x[N - 2]);
+    }
+
+    for(int i = 0; i < N - 1; i++){
+        if(x[i] <= x0 && x0 <= x[i + 1]){
+            return f[i] + ((f[i + 1] - f[i])/(x[i + 1] - x[i])) * (x0 - x[i]);
+        }
+    }
+    return -1;
+
 }
 
 
