c8088c1a906b301b5293f54ff48f79e4efd8294e
adriraw <adrianrijorijo@gmail.com>
2026-06-02 21:13:02 +0100

    3.1 hecho - Interpolacion

--- Tema3_Interpolacion_I/P31_vecino_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P31_vecino_hacer_alumno/mn_interpolacion.cpp
@@ -11,16 +11,16 @@ real x0)  /// punto donde se evalua la función interpolada
 
     int N = x.dim();
 
-    if(x0 <= x[0]) return f[0];
+    if(x0 <= x[0]) return f[0];  ///Se comprueba si esta por debajo del primer valor
 
-    if(x0 >= x[N - 1]) return f[N - 1];
+    if(x0 >= x[N - 1]) return f[N - 1]; ///Se comprueba si el valor esta por encima del ultimo valor
 
     for(int i = 0; i < N - 1; i++){
-        if(x[i] <= x0 && x0 <= x[i + 1]){
-            if(mn_abs(x0 - x[i]) <= mn_abs(x0 - x[i + 1])){
-                return f[i];
+            if(mn_abs(x0 - x[i]) <= mn_abs(x0 - x[i + 1])){ ///S
+        if(x[i] <= x0 && x0 <= x[i + 1]){   ///Se comprueba si x0 esta entre el valor actual y el siguiente
+                return f[i];    ///Si es menor es menor que el siguiente se devuelve el valor actual
             } else {
-                return f[i + 1];
+                return f[i + 1];    ///Si es mayor se devuelve el siguiente
             }
         }
     }
