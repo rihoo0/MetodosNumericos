afb6ff184bbc996f0fc3aa0d2ca9ca93b308e012
adriraw <adrianrijorijo@gmail.com>
2026-06-25 22:40:44 +0100

    EXTRA REPASO 5.1 - HECHO

--- Tema5_DiferenciacionIntegracion/P51_Mascara2D_hacer_alumno/mn_mascara3x3.cpp
+++ Tema5_DiferenciacionIntegracion/P51_Mascara2D_hacer_alumno/mn_mascara3x3.cpp
@@ -11,6 +11,49 @@ Array2D< real > &F, /// función tabulada
 Array2D< real > &m) /// coeficientes máscara 3x3
 {
   /// HACER ALUMNO
+    if(F.dim1()<1 || F.dim2()<1 || m.dim1()!=3 || m.dim2()!=3) return Array2D< real >();
+
+  Array2D< real > M(F.dim1(),F.dim2(),0.);
+
+  for(int i=1;i<M.dim1()-1;i++){
+    for(int j=1;j<M.dim2()-1;j++){
+      for(int k=-1;k<2;k++){
+        for(int l=-1;l<2;l++){
+          M[i][j]+=m[k+1][l+1]*F[k+i][l+j];
+        }
+      }
+    }
+  }
+
+   for(int j=1;j<M.dim2()-1;j++){
+      for(int k=-1;k<2;k++){
+        for(int l=-1;l<2;l++){
+          M[0][j]+=m[k+1][l+1]*F[max(k,0)][l+j];
+          M[M.dim1()-1][j]+=m[k+1][l+1]*F[M.dim1()-1+min(k,0)][l+j];
+        }
+      }
+  }
+
+  for(int i=1;i<M.dim1()-1;i++){
+      for(int k=-1;k<2;k++){
+        for(int l=-1;l<2;l++){
+          M[i][0]+=m[k+1][l+1]*F[k+i][max(l,0)];
+          M[i][M.dim2()-1]+=m[k+1][l+1]*F[k+i][M.dim2()-1+min(l,0)];
+        }
+      }
+  }
+
+  for(int k=-1;k<2;k++){
+    for(int l=-1;l<2;l++){
+      M[M.dim1()-1][M.dim2()-1]+=m[k+1][l+1]*F[M.dim1()-1+min(k,0)][M.dim2()-1+min(l,0)];
+      M[0][M.dim2()-1]+=m[k+1][l+1]*F[max(k,0)][M.dim2()-1+min(l,0)];
+      M[M.dim1()-1][0]+=m[k+1][l+1]*F[M.dim1()-1+min(k,0)][max(l,0)];
+      M[0][0]+=m[k+1][l+1]*F[max(k,0)][max(l,0)];
+    }
+  }
+
+
+  return M;
 
 }
 
