9c20b35c74b56e1b02587a50f2c7d0a04604c269
adriraw <adrianrijorijo@gmail.com>
2026-06-25 02:05:55 +0100

    EXTRA REPASO 3.7 NEWTON - HECHO(REPASAR)

--- Tema3_Interpolacion_I/P37_DiferenciasNewton_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P37_DiferenciasNewton_hacer_alumno/mn_interpolacion.cpp
@@ -10,6 +10,23 @@ const Array1D< real > &F) /// VECTOR DE VALORES DE LA FUNCION EN LOS PUNTOS DE I
 {
   /// HACER ALUMNO
 
+  Array1D<real> A(X.dim());
+
+  Array1D<real> B(F.dim());
+  for(int i = 0; i < F.dim(); i++){
+    B[i] = F[i];
+  }
+  A[0] = B[0];
+
+  for(int k = 1; k < X.dim(); k++){
+    for(int l = 0; l < X.dim() - k; l++){
+            if(X[k + l] == X[l]) return Array1D<real>();
+        B[l] = (B[l + 1] - B[l]) / (X[k + l] - X[l]);
+    }
+    A[k] = B[0];
+  }
+  return A;
+
 }
 
 
@@ -20,7 +37,12 @@ const Array1D< real > &X, /// VECTOR CON LOS VALORES DE LOS PUNTOS DE INTERPOLAC
 const real x0) /// VALOR DONDE SE INTERPOLA EL POLINOMIO
 {
   /// HACER ALUMNO
-
+    int n = A.dim();
+    real b = A[n - 1];
+    for(int i = n - 2; i >= 0; i--){
+        b = A[i] + b * (x0 - X[i]);
+    }
+    return b;
 }
 
 
