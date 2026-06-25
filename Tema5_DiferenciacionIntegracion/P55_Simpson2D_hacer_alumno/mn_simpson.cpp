1232e6bea69bf9bbbdff14c6653c96e06f347506
adriraw <adrianrijorijo@gmail.com>
2026-06-25 23:18:50 +0100

    EXTRA REPASO 5.5 SIMPSON 2D - HECHO

--- Tema5_DiferenciacionIntegracion/P55_Simpson2D_hacer_alumno/mn_simpson.cpp
+++ Tema5_DiferenciacionIntegracion/P55_Simpson2D_hacer_alumno/mn_simpson.cpp
@@ -15,7 +15,26 @@ real d /** extremo derecho del invervalo y */,
 int M /** numero de intervalos en x para calcular la integral */,
 int N /** numero de intervalos en y para calcular la integral */)
 {
-   /** HACER ALUMNO.*/
+    real hx = (b-a) / M;
+    real hy = (d-c) / N;
+    real sum = 0.;
 
+    for(int k = 0; k <= M; k++){
+        real x = a + hx*k;
+        real wx;
+        if(k == 0 || k == M)    wx = 1.;
+        else if (k%2 == 0)      wx = 2.;
+        else                    wx = 4.;
+
+        for(int n = 0; n <= N; n++){
+            real y = c + hy * n;
+            real wy;
+            if(n == 0 || n == N)    wy = 1.;
+            else if(n%2 == 0)       wy = 2.;
+            else                    wy = 4.;
+            sum += F(x, y) * wx * wy;
+        }
+    }
+    return sum * hy * hx /9.;
 }
 
