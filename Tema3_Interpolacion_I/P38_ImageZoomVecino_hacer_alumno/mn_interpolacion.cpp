61c9c2474d954ef7b13baf8fbadb4f0facccaa83
adriraw <adrianrijorijo@gmail.com>
2026-06-25 02:22:34 +0100

    EXTRA REPASO ZOOMVECINO - HECHO REPASO

--- Tema3_Interpolacion_I/P38_ImageZoomVecino_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P38_ImageZoomVecino_hacer_alumno/mn_interpolacion.cpp
@@ -13,4 +13,20 @@ real z /** FACTOR DE ZOOM */)
 {
   /// HACER ALUMNO
 
+  Array2D<real> F(z * A.dim1(), z*A.dim2());
+  int dF1 = F.dim1();
+  int dF2 = F.dim2();
+
+  for(int ip = 0; ip < dF1; ip++){
+    for(int jp = 0; jp < dF2; jp++){
+        real x = ip/z;
+        real y = jp/z;
+        int i = x;
+        int j = y;
+        if((x-i)>0.5 && i+1<A.dim1()) i=i+1;
+        if((y-j)>0.5 && j+1<A.dim2()) j=j+1;
+        F[ip][jp] = A[i][j];
+    }
+  }
+  return F;
 }
